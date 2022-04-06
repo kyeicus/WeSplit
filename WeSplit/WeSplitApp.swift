207d0ca020df2d26dd0898885e48e5eefc085ac7
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-06 01:25:29 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Emmanuel K. Nketia on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
